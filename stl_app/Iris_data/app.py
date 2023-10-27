d313f21b51b2be22df8d5244b4c1661803306007
comseven <66056040@kmitl.ac.th>
2023-10-27 15:20:00 +0700

    Explore iris data

---
+++ stl_app/Iris_data/app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot  as plt
+import seaborn as sns
+
+st.title("Iris ")
+st.markdown('สร้าง `scatter plot` แสดงผลข้อมูล **Palmer\'s Penguins** กัน แบบเดียวกับ **Iris dataset**')
+
+choices = ['sepal.length',
+           'sepal.width',
+           'petal.length',
+           'petal.width']
+
+selected_x_var = st.selectbox('เลือก แกน x', (choices))
+selected_y_var = st.selectbox('เลือก แกน y', (choices))
+
+iris_file = st.file_uploader("เลือกไฟล์ CSV", type=['csv'])
+
+
+
+
+if iris_file is not None:
+    iris_df = pd.read_csv(iris_file)
+else:
+    st.stop()
+
+st.subheader('ข้อมูลตัวอย่าง')
+st.write(iris_df)
+
+st.subheader('แสดงผลข้อมูล')
+sns.set_style('darkgrid')
+markers = {"Setosa": "v", "Versicolor": "s", "Virginica": 'o'}
+
+fig, ax = plt.subplots()
+ax = sns.scatterplot(data=iris_df,
+                     x=selected_x_var, y=selected_y_var,
+                     hue='variety', markers=markers, style='variety')
+plt.xlabel(selected_x_var)
+plt.ylabel(selected_y_var)
+plt.title("Palmer's Penguins Data")
+st.pyplot(fig)

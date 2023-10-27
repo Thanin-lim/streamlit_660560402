ab85c48d6cc99160a0d5d7229efc525711856b28
comseven <66056040@kmitl.ac.th>
2023-10-27 14:27:30 +0700

    Add selectbox

--- stl_app/03_penguin_data/app.py
+++ stl_app/03_penguin_data/app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib as plt
 import seaborn as sns
-
 st.title("Palmer's Penguins")
 st.markdown('สร้าง `scatter plot` แสดงผลข้อมูล **Palmer\'s Penguins** กัน แบบเดียวกับ **Iris dataset**')
 
@@ -13,8 +12,8 @@ choices = ['bill_length_mm',
 
 # https://docs.streamlit.io/library/api-reference/widgets/st.selectbox
 # 1. สร้าง st.selectbox ของ ตัวเลือก แกน x และ y จาก choices
-selected_x_var = 'อะไรดี'
-selected_y_var = 'อะไรดี'
+selected_x_var = st.selectbox('เลือก แกน x', (choices))
+selected_y_var =  st.selectbox('เลือก แกน y', (choices))
 
 # https://docs.streamlit.io/library/api-reference/widgets/st.file_uploader
 # 2. สร้าง st.file_uploader เพื่อให้เลือกไฟล์ .csv เท่านั้น จากเครื่องผู้ใช้งาน

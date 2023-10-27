d51fb5aa26d5b4099e0685086b29765be62551a0
comseven <66056040@kmitl.ac.th>
2023-10-27 14:19:25 +0700

    Add

--- stl_app/02_persistence_data/app.py
+++ stl_app/02_persistence_data/app.py
@@ -1,16 +1,17 @@
 import streamlit as st
 
 st.title('My To-Do List Creator')
-my_todo_list = ["Learn Markdown", "Learn Python", "Learn Streamlit"]
-st.write('My current To-Do list is:', my_todo_list)
+if 'my_todo_list' not in st.session_state:
+    st.session_state.my_todo_list = [{"Learn Markdown":1}, {"Learn Python":2}, {"Learn Streamlit":4}]
+st.write('My current To-Do list is:', st.session_state.my_todo_list)
 
 new_todo = st.text_input("What do you need to do?")
-
+day_todo = st.number_input('Day',min_value=1,max_value=20)
 if st.button('Add the new To-Do item'):
     st.write('Adding a new item to the list')
-    my_todo_list.append(new_todo)
+    st.session_state.my_todo_list.append({new_todo:day_todo})
 
-st.write('My new To-Do list is:', my_todo_list)
+st.write('My new To-Do list is:', st.session_state.my_todo_list)
 
 # TODO
 # 1. make it save to session state
